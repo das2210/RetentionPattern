ad10da2b74e05f11a0237f75c1016a9186033ed6
Dacia Simon <112356414+das2210@users.noreply.github.com>
2025-01-09 15:17:41 -0500

    Update RetentionPattern.java

--- RetentionPattern.java
+++ RetentionPattern.java
@@ -1,10 +1,27 @@
+import java.util.*;
+import java.io.*;
+
 public class RetentionPattern{
 
+	public static void main(String[] args) {
+      
+	/* load csv file into this java program */
+    
+		   Scanner user_input = new Scanner(System.in);
+		   System.out.println("what's the name of your csv file?\n");
+		   String file_name = user_input.next();
+		   
+		   try {
+		   File file = new File(file_name);
+		   Scanner input = new Scanner(file);
+		   System.out.println("you entered:\n" + input);
+		   }
+		   
+		   catch(IOException e){
+			   System.out.print("file does not exist");
+			   System.exit(1); 
+		   }
 
-
-
-
-
-
-
-}
+      
+    }
+	}
